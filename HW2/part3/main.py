e38bcfafdae8e7bec89e5e128999cc1fd4af393a
Ismayil Ismayilov <iismayilov4574@ada.edu.az>
2021-11-21 12:18:52 +0300

    Implemented Part 3. Clean up needed

--- HW2/part3/main.py
+++ HW2/part3/main.py
@@ -1,11 +1,8 @@
-import math
-import random
+from collections import Counter
 
+import numpy as np
 
-""" Globals """
-DOMAIN = list(range(25))  # [0, 1, ..., 24]
-
-""" Helpers """
+DOMAIN = list(range(25))
 
 
 def read_dataset(filename):
@@ -24,14 +21,21 @@ def read_dataset(filename):
             result.append(int(line))
     return result
 
-# You can define your own helper functions here. #
 
-### HELPERS END ###
+def get_counts(dataset):
+    counts = Counter(dataset)
+    return [counts[hour] for hour in DOMAIN]
 
 
-""" Functions to implement """
+def calculate_average_error(actual_hist, noisy_hist):
+    num_bins = len(actual_hist)
 
-# TODO: Implement this function!
+    return sum([abs(actual_val - noise_val)
+                for actual_val, noise_val in zip(actual_hist, noisy_hist)]) / num_bins
+
+
+def apply_estimator(hist, val, n, p, q):
+    return (hist[val] - n * q) / (p - q)
 
 
 def perturb_grr(val, epsilon):
@@ -45,9 +49,14 @@ def perturb_grr(val, epsilon):
             Perturbed value that the user reports to the server
     """
     d = len(DOMAIN)
-    pass
 
-# TODO: Implement this function!
+    p = np.exp(epsilon) / (np.exp(epsilon) + d - 1)
+
+    if np.random.uniform() < p:
+        return val
+    else:
+        domain_without_val = [elem for elem in DOMAIN if elem != val]
+        return np.random.choice(domain_without_val)
 
 
 def estimate_grr(perturbed_values, epsilon):
@@ -61,9 +70,18 @@ def estimate_grr(perturbed_values, epsilon):
             Estimated histogram as a list: [1.5, 6.7, ..., 1061.0] 
             for each hour in the domain [0, 1, ..., 24] respectively.
     """
-    pass
+    d = len(DOMAIN)
 
-# TODO: Implement this function!
+    p = np.exp(epsilon) / (np.exp(epsilon) + d - 1)
+    q = (1 - p) / (d - 1)
+
+    n = len(perturbed_values)
+    noisy_hist = get_counts(perturbed_values)
+
+    denoised_hist = [apply_estimator(
+        noisy_hist, val, n, p, q) for val in DOMAIN]
+
+    return denoised_hist
 
 
 def grr_experiment(dataset, epsilon):
@@ -76,9 +94,15 @@ def grr_experiment(dataset, epsilon):
         Returns:
             Error of the estimated histogram (float) -> Ex: 330.78
     """
-    return 0.0
 
-# TODO: Implement this function!
+    actual_hist = get_counts(dataset)
+
+    perturbed_values = [perturb_grr(val, epsilon) for val in dataset]
+    denoised_hist = estimate_grr(perturbed_values, epsilon)
+
+    error = calculate_average_error(actual_hist, denoised_hist)
+
+    return error
 
 
 def encode_rappor(val):
@@ -90,9 +114,7 @@ def encode_rappor(val):
         Returns:
             The encoded bit vector as a list: [0, 1, ..., 0]
     """
-    pass
-
-# TODO: Implement this function!
+    return [int(val == elem) for elem in DOMAIN]
 
 
 def perturb_rappor(encoded_val, epsilon):
@@ -105,9 +127,17 @@ def perturb_rappor(encoded_val, epsilon):
         Returns:
             Perturbed bit vector that the user reports to the server as a list: [1, 1, ..., 0]
     """
-    pass
 
-# TODO: Implement this function!
+    p = (np.exp(epsilon / 2)) / (np.exp(epsilon / 2) + 1)
+
+    def perturb_bit(bit):
+        is_bit_preserved = np.random.uniform() < p
+
+        return bit if is_bit_preserved else abs(1 - bit)
+
+    perturbed_vector = [perturb_bit(bit) for bit in encoded_val]
+
+    return perturbed_vector
 
 
 def estimate_rappor(perturbed_values, epsilon):
@@ -121,7 +151,17 @@ def estimate_rappor(perturbed_values, epsilon):
             Estimated histogram as a list: [1.5, 6.7, ..., 1061.0] 
             for each hour in the domain [0, 1, ..., 24] respectively.
     """
-    pass
+
+    p = (np.exp(epsilon / 2)) / (np.exp(epsilon / 2) + 1)
+    q = 1 - p
+
+    n = len(perturbed_values)
+    noisy_hist = [sum(index_vals) for index_vals in zip(*perturbed_values)]
+
+    denoised_hist = [apply_estimator(
+        noisy_hist, val, n, p, q) for val in DOMAIN]
+
+    return denoised_hist
 
 # TODO: Implement this function!
 
@@ -136,14 +176,21 @@ def rappor_experiment(dataset, epsilon):
         Returns:
             Error of the estimated histogram (float) -> Ex: 330.78
     """
-    return 0.0
+    actual_hist = get_counts(dataset)
+
+    encoded_values = [encode_rappor(val) for val in dataset]
+    perturbed_values = [perturb_rappor(val, epsilon) for val in encoded_values]
+    denoised_hist = estimate_rappor(perturbed_values, epsilon)
+
+    error = calculate_average_error(actual_hist, denoised_hist)
+
+    return error
 
 
 def main():
     dataset = read_dataset("daily_time.txt")
 
     print("GRR EXPERIMENT")
-    # for epsilon in [20.0]:
     for epsilon in [0.1, 0.5, 1.0, 2.0, 4.0, 6.0]:
         error = grr_experiment(dataset, epsilon)
         print("e={}, Error: {:.2f}".format(epsilon, error))
